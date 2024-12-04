df49891248563a70f76eea481c12440cfd00cd19
Weztwind <1367825762@qq.com>
2024-12-04 10:31:21 +0800

    2024/12/04： 更改目录结构

--- DM/DDPM/Unet/tools/_init_.py
+++
@@ -1,9 +0,0 @@
-__all__ = []
-
-# 可选：添加版本信息
-__version__ = '1.0.0'
-__last_update__ = '2024/12/1'
-
-# 可选：添加包的元信息
-__author__ = 'Weztwind'
-__description__ = 'Model implementations for DDPM of Unet architecture'
